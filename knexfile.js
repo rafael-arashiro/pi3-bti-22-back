0394b92711ff897d4739c7b215475f9405687dc9
rafael-arashiro <rafael_arashiro1@yahoo.com.br>
2025-04-07 03:49:24 -0300

    update

--- knexfile.js
+++ knexfile.js
@@ -5,6 +5,7 @@ module.exports = {
     client: 'mysql2',
     connection: {
       host: process.env.SERVER,
+      port: process.env.PORT,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB,
